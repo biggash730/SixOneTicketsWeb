bd0245289698365e3b8a41c319a45afb663a34e6
Francis Miah <francis@axoninfosystems.com>
2018-04-30 21:27:46 +0000

    jc

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off 
 echo ----- Run webpack and copy bundle files ------
-webpack && xcopy build\bundle.js ..\corrupack-api\corrupack\scripts /s /e /i /h /y
+webpack && xcopy build\bundle.js ..\61tikitsapi\SixOneTikitsApi\scripts /s /e /i /h /y && xcopy build\bundle.js.map ..\61tikitsapi\SixOneTikitsApi\scripts /s /e /i /h /y
