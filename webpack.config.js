4d8b4376628e0744c2fbc6443de3bcb1c792d423
Francis Miah <francis@axoninfosystems.com>
2018-04-30 14:33:54 +0000

    jc

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   devServer: {
       proxy: [{
           path: '/api/**',
-          target: 'http://127.0.0.1:1305'
+          target: 'http://127.0.0.1:5051'
       }]
   }  
 }
